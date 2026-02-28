49ce9d9c2ece8c8810baae1e1dbab7297295255a
Grant Jenks <grant.jenks@gmail.com>
2026-02-27 20:10:50 -0800

    todo: expand descriptor-rebound builtin __self__ tests

--- tests/test_sandbox_security.py
+++ tests/test_sandbox_security.py
@@ -6961,6 +6961,66 @@ RESULT = getter("__self__")
         )
 
 
+def test_descriptor_rebound_bound_getattribute_keyword_name_cannot_reach_builtin_self():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+getter = len.__getattribute__.__get__(None, type(len))
+RESULT = getter(name="__self__")
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<descriptor_rebound_bound_getattribute_builtin_self_keyword_probe>",
+        )
+
+
+def test_descriptor_rebound_bound_getattribute_keyword_key_cannot_reach_builtin_self():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+getter = len.__getattribute__.__get__(None, type(len))
+RESULT = getter(**{"name": "__self__"})
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<descriptor_rebound_bound_getattribute_builtin_self_keyword_key_probe>",
+        )
+
+
+def test_stateful_str_subclass_keyword_key_cannot_bypass_descriptor_rebound_bound_getattribute_builtin_self_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.eq_calls = 0
+        return obj
+
+    __hash__ = str.__hash__
+
+    def __eq__(self, other):
+        if isinstance(other, str) and other == "name":
+            self.eq_calls += 1
+            return self.eq_calls > 1
+        return str.__eq__(self, other)
+
+getter = len.__getattribute__.__get__(None, type(len))
+key = Sneaky("name")
+RESULT = getter(**{key: "__self__"})
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_keyword_key_descriptor_rebound_bound_getattribute_builtin_self_probe>",
+        )
+
+
 def test_str_subclass_str_override_keyword_name_cannot_bypass_descriptor_rebound_bound_getattribute_builtin_self_guard():
     interp = Interpreter(allowed_imports=set())
     env = interp.make_default_env()
