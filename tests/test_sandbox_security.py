938d95e4d1991fa819a8a1306ff359dc83078bc0
Grant Jenks <grant.jenks@gmail.com>
2026-02-27 18:33:53 -0800

    todo: add positional bound function globals regressions

--- tests/test_sandbox_security.py
+++ tests/test_sandbox_security.py
@@ -6039,6 +6039,60 @@ RESULT = getter("__globals__")["__builtins__"]
         )
 
 
+def test_stateful_str_subclass_positional_name_cannot_bypass_bound_getattribute_function_globals_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.calls = 0
+        return obj
+
+    def __hash__(self):
+        self.calls += 1
+        if self.calls <= 1:
+            return 0
+        return str.__hash__(self)
+
+def probe():
+    return 1
+
+getter = probe.__getattribute__
+name = Sneaky("__globals__")
+RESULT = getter(name)["__builtins__"]
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_positional_bound_getattribute_function_globals_probe>",
+        )
+
+
+def test_str_subclass_str_override_positional_name_cannot_bypass_bound_getattribute_function_globals_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __str__(self):
+        return "not_name"
+
+def probe():
+    return 1
+
+getter = probe.__getattribute__
+name = Sneaky("__globals__")
+RESULT = getter(name)["__builtins__"]
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<str_override_positional_bound_getattribute_function_globals_probe>",
+        )
+
+
 def test_stateful_str_subclass_keyword_name_cannot_bypass_bound_getattribute_function_globals_guard():
     interp = Interpreter(allowed_imports=set())
     env = interp.make_default_env()
