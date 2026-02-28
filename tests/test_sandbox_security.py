2c7b9d83523caf4a11053dc5d75fd566cd8f3505
Grant Jenks <grant.jenks@gmail.com>
2026-02-27 17:22:56 -0800

    todo: add __mro__ keyword bypass regressions

--- tests/test_sandbox_security.py
+++ tests/test_sandbox_security.py
@@ -1636,6 +1636,89 @@ RESULT = getter("__mro__")
         interp.run(source, env=env, filename="<super_getattribute_class_mro_probe>")
 
 
+def test_stateful_str_subclass_keyword_name_cannot_bypass_object_getattribute_class_mro_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.calls = 0
+        return obj
+
+    def __hash__(self):
+        self.calls += 1
+        if self.calls <= 1:
+            return 0
+        return str.__hash__(self)
+
+name = Sneaky("__mro__")
+RESULT = object.__getattribute__(Probe, name=name)
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_keyword_object_getattribute_class_mro_probe>",
+        )
+
+
+def test_stateful_str_subclass_keyword_name_cannot_bypass_type_getattribute_class_mro_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.calls = 0
+        return obj
+
+    def __hash__(self):
+        self.calls += 1
+        if self.calls <= 1:
+            return 0
+        return str.__hash__(self)
+
+name = Sneaky("__mro__")
+RESULT = type.__getattribute__(Probe, name=name)
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_keyword_type_getattribute_class_mro_probe>",
+        )
+
+
+def test_str_subclass_str_override_keyword_name_cannot_bypass_super_getattribute_class_mro_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+class Sneaky(str):
+    def __str__(self):
+        return "not_blocked"
+
+getter = super(type(Probe), Probe).__getattribute__
+name = Sneaky("__mro__")
+RESULT = getter(name=name)
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<str_override_keyword_super_getattribute_class_mro_probe>",
+        )
+
+
 def test_function_closure_cell_escape_chain_is_blocked():
     interp = Interpreter(allowed_imports=set())
     env = interp.make_default_env()
