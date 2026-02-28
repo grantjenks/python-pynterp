4f8ce71563df4c8cfb7ef0956913d4212921348d
Grant Jenks <grant.jenks@gmail.com>
2026-02-27 16:25:47 -0800

    todo: add class base pivot regressions

--- tests/test_sandbox_security.py
+++ tests/test_sandbox_security.py
@@ -513,6 +513,61 @@ RESULT = getter(Probe, "__subclasses__")
         interp.run(source, env=env, filename="<metaclass_getattribute_probe>")
 
 
+def test_class_base_escape_chain_is_blocked():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+RESULT = Probe.__base__
+"""
+    with pytest.raises(AttributeError):
+        interp.run(source, env=env, filename="<class_base_probe>")
+
+
+def test_object_getattribute_cannot_reach_class_base():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+getter = object.__getattribute__
+RESULT = getter(Probe, "__base__")
+"""
+    with pytest.raises(AttributeError):
+        interp.run(source, env=env, filename="<object_getattribute_class_base_probe>")
+
+
+def test_type_getattribute_cannot_reach_class_base():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+getter = type.__getattribute__
+RESULT = getter(Probe, "__base__")
+"""
+    with pytest.raises(AttributeError):
+        interp.run(source, env=env, filename="<type_getattribute_class_base_probe>")
+
+
+def test_super_getattribute_cannot_reach_class_base():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Probe:
+    pass
+
+getter = super(type(Probe), Probe).__getattribute__
+RESULT = getter("__base__")
+"""
+    with pytest.raises(AttributeError):
+        interp.run(source, env=env, filename="<super_getattribute_class_base_probe>")
+
+
 def test_function_closure_cell_escape_chain_is_blocked():
     interp = Interpreter(allowed_imports=set())
     env = interp.make_default_env()
