a89f97e54b9e4b60b27f2fda1ddd96030413b255
Grant Jenks <grant.jenks@gmail.com>
2026-02-27 17:08:02 -0800

    todo: add keyword module metadata guard regressions

--- tests/test_sandbox_security.py
+++ tests/test_sandbox_security.py
@@ -1882,3 +1882,83 @@ RESULT = getter(name=name)
             env=env,
             filename="<str_override_super_getattribute_module_loader_probe>",
         )
+
+
+def test_stateful_str_subclass_keyword_name_cannot_bypass_object_getattribute_module_loader_guard():
+    interp = Interpreter(allowed_imports={"math"})
+    env = interp.make_default_env()
+    source = """
+import math
+
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.calls = 0
+        return obj
+
+    def __hash__(self):
+        self.calls += 1
+        if self.calls <= 1:
+            return 0
+        return str.__hash__(self)
+
+name = Sneaky("__loader__")
+RESULT = object.__getattribute__(math, name=name)
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_keyword_object_getattribute_module_loader_probe>",
+        )
+
+
+def test_stateful_str_subclass_keyword_name_cannot_bypass_super_getattribute_module_spec_guard():
+    interp = Interpreter(allowed_imports={"math"})
+    env = interp.make_default_env()
+    source = """
+import math
+
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.calls = 0
+        return obj
+
+    def __hash__(self):
+        self.calls += 1
+        if self.calls <= 1:
+            return 0
+        return str.__hash__(self)
+
+getter = super(type(math), math).__getattribute__
+name = Sneaky("__spec__")
+RESULT = getter(name=name)
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_keyword_super_getattribute_module_spec_probe>",
+        )
+
+
+def test_str_subclass_str_override_keyword_name_cannot_bypass_type_getattribute_module_loader_guard():
+    interp = Interpreter(allowed_imports={"math"})
+    env = interp.make_default_env()
+    source = """
+import math
+
+class Sneaky(str):
+    def __str__(self):
+        return "not_blocked"
+
+name = Sneaky("__loader__")
+RESULT = type.__getattribute__(math, name=name)
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<str_override_keyword_type_getattribute_module_loader_probe>",
+        )
