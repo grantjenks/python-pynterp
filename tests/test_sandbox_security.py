6e6bfbb2a175ceae943c67983e25da6ff0e7f8e7
Grant Jenks <grant.jenks@gmail.com>
2026-02-27 20:17:15 -0800

    todo: expand descriptor-rebound function-globals tests

--- tests/test_sandbox_security.py
+++ tests/test_sandbox_security.py
@@ -6814,6 +6814,118 @@ RESULT = getter(**{key: "__globals__"})["__builtins__"]
         )
 
 
+def test_stateful_str_subclass_positional_name_cannot_bypass_descriptor_rebound_bound_getattribute_function_globals_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.eq_calls = 0
+        return obj
+
+    __hash__ = str.__hash__
+
+    def __eq__(self, other):
+        if isinstance(other, str) and other == "__globals__":
+            self.eq_calls += 1
+            return self.eq_calls > 1
+        return str.__eq__(self, other)
+
+def probe():
+    return 1
+
+getter = probe.__getattribute__.__get__(None, type(probe))
+name = Sneaky("__globals__")
+RESULT = getter(name)["__builtins__"]
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_positional_descriptor_rebound_bound_getattribute_function_globals_probe>",
+        )
+
+
+def test_str_subclass_str_override_positional_name_cannot_bypass_descriptor_rebound_bound_getattribute_function_globals_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __str__(self):
+        return "not_globals"
+
+def probe():
+    return 1
+
+getter = probe.__getattribute__.__get__(None, type(probe))
+name = Sneaky("__globals__")
+RESULT = getter(name)["__builtins__"]
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<str_override_positional_descriptor_rebound_bound_getattribute_function_globals_probe>",
+        )
+
+
+def test_stateful_str_subclass_keyword_key_cannot_bypass_descriptor_rebound_bound_getattribute_function_globals_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __new__(cls, value):
+        obj = str.__new__(cls, value)
+        obj.eq_calls = 0
+        return obj
+
+    __hash__ = str.__hash__
+
+    def __eq__(self, other):
+        if isinstance(other, str) and other == "name":
+            self.eq_calls += 1
+            return self.eq_calls > 1
+        return str.__eq__(self, other)
+
+def probe():
+    return 1
+
+getter = probe.__getattribute__.__get__(None, type(probe))
+key = Sneaky("name")
+RESULT = getter(**{key: "__globals__"})["__builtins__"]
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<stateful_str_keyword_key_descriptor_rebound_bound_getattribute_function_globals_probe>",
+        )
+
+
+def test_str_subclass_str_override_keyword_name_cannot_bypass_descriptor_rebound_bound_getattribute_function_globals_guard():
+    interp = Interpreter(allowed_imports=set())
+    env = interp.make_default_env()
+    source = """
+class Sneaky(str):
+    def __str__(self):
+        return "not_globals"
+
+def probe():
+    return 1
+
+getter = probe.__getattribute__.__get__(None, type(probe))
+name = Sneaky("__globals__")
+RESULT = getter(name=name)["__builtins__"]
+"""
+    with pytest.raises(AttributeError):
+        interp.run(
+            source,
+            env=env,
+            filename="<str_override_keyword_descriptor_rebound_bound_getattribute_function_globals_probe>",
+        )
+
+
 def test_stateful_str_subclass_keyword_key_cannot_bypass_descriptor_rebound_bound_getattribute_importer_self_guard():
     interp = Interpreter(allowed_imports=set())
     env = interp.make_default_env()
